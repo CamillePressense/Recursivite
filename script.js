2a3fee192e320742bbd62e78439d99207ed57d58
Pressense Fasani <camillefasani@gmail.com>
2025-05-06 15:03:00 +0200

    Etape 2 : fonction afficherDossierIteratif

--- script.js
+++ script.js
@@ -5,9 +5,33 @@ import { dossierPrincipal } from "./constant.js";
 //Etape 1
 //Crée une fonction afficherDossier qui va afficher le nom du dossier principal en utilisant la variable dossierPrincipal
 
-function afficherDossier() {
-    console.log("📁 " + dossierPrincipal.nom)
+function afficherDossier(dossier) {
+    console.log(`🗂️  ${dossier.nom}`);
 }
 
-afficherDossier()
+//Etape 2
+//Tu vas créer deux fonctions : afficherDossierIteratif (l’utilisation des instructions for… est autorisée)
+// et afficherDossierRecursif (il faut que cette fonction s’appelle elle-même!)
+//Ces deux fonctions afficheront les dossiers et le fichier que contient le dossier Ada.
+
+function afficherFichier(fichier){
+    console.log(`📑 ${fichier.nom}`);
+}
+
+function afficherDossierIteratif(){
+    afficherDossier(dossierPrincipal)
+    const contenuDossier = dossierPrincipal.contenu;
+    for (const element in contenuDossier){
+        if (contenuDossier[element].contenu){
+            afficherDossier(contenuDossier[element]);
+        } else {
+            afficherFichier(contenuDossier[element]);
+        }     
+    }
+}
+
+
+afficherDossierIteratif()
+
+
 
