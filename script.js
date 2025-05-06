5a5c10567bd1914eef1253329889743b27854c7d
Pressense Fasani <camillefasani@gmail.com>
2025-05-06 11:28:55 +0200

    Etape 1

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 import { dossierPrincipal } from "./constant.js";
 
-console.log (dossierPrincipal);
+//console.log (dossierPrincipal);
+
+//Etape 1
+//Crée une fonction afficherDossier qui va afficher le nom du dossier principal en utilisant la variable dossierPrincipal
+
+function afficherDossier() {
+    console.log(dossierPrincipal.nom)
+}
+
+afficherDossier()
